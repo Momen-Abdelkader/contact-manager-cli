b47960b219715a8b0df06cdc3e9af7b1d9932ac8
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2026-02-24 03:19:35 +0200

    feat: add IRepository interface

--- ContactManagerCLI/Repositories/IRepository.cs
+++ ContactManagerCLI/Repositories/IRepository.cs
@@ -2,5 +2,11 @@ namespace ContactManagerCLI.Repositories;
 
 public interface IRepository<T>
 {
-    // TODO: implement
+    List<T> GetAll();
+    T? GetById(int id);
+    IEnumerable<T> Find(Func<T, bool> predicate);
+    void Add(T item);
+    void Update(T item);
+    void Delete(int id);
+    void SaveAll();
 }
