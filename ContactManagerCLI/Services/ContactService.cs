c7e4901621c07b8010328bc2acff05875f7aa7ba
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2026-02-24 07:21:09 +0200

    feat: add ContactService

---
+++ ContactManagerCLI/Services/ContactService.cs
@@ -0,0 +1,37 @@
+using ContactManagerCLI.Models;
+using ContactManagerCLI.Repositories;
+
+namespace ContactManagerCLI.Services;
+
+public class ContactService : IContactService
+{
+    private readonly IRepository<Contact> _repository;
+
+    public ContactService(IRepository<Contact> repository)
+    {
+        _repository = repository;
+    }
+
+    public List<Contact> GetAllContacts() => _repository.GetAll();
+
+    public Contact? GetContactById(Guid id) => _repository.GetById(id);
+
+    public IEnumerable<Contact> Search(string query)
+    {
+        return _repository.Find(c =>
+                                c.Name.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+                                c.Email.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+                                c.PhoneNumber.Contains(query, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public IEnumerable<Contact> Filter(Func<Contact, bool> predicate) => _repository.Find(predicate);
+
+    public void AddContact(Contact contact) => _repository.Add(contact);
+
+    public void UpdateContact(Contact contact) => _repository.Update(contact);
+
+    public void DeleteContact(Guid id) => _repository.Delete(id);
+
+    public void SaveAll() => _repository.SaveAll();
+}
+
