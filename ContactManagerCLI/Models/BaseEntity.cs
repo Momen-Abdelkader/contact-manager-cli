d99011083a8427751ed256a6fffdfe898f25a0c2
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2026-02-24 06:02:31 +0200

    feat: add BaseEntity

---
+++ ContactManagerCLI/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace ContactManagerCLI.Models;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public DateTimeOffset CreatedAt { get; init; } = DateTimeOffset.Now;
+}
--- ContactManagerCLI/Models/Contact.cs
+++ ContactManagerCLI/Models/Contact.cs
@@ -1,10 +1,8 @@
 namespace ContactManagerCLI.Models;
 
-public class Contact(string name, string email, string phoneNumber)
+public class Contact(string name, string email, string phoneNumber) : BaseEntity
 {
-    public Guid Id { get; } = Guid.NewGuid();
     public string Name { get; set; } = name;
     public string Email { get; set; } = email;
     public string PhoneNumber { get; set; } = phoneNumber;
-    public DateTimeOffset CreatedAt { get; } = DateTimeOffset.Now;
 }
